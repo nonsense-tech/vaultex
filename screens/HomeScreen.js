fd26bc16ce7713792cd76020f3f717d5808d1a9b
Max Alekseenko <nnwen0609@gmail.com>
2019-10-26 17:41:40 +0300

    add exchange rates

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,11 +5,12 @@ import { Text, Button, Container, Header, Content, Body, Icon, Input, Item } fro
 import RNPickerSelect from 'react-native-picker-select';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import Web3 from 'web3';
-import { fromWei, toHex } from 'web3-utils';
+import { fromWei, toHex, BN, toWei } from 'web3-utils';
 
 import addresses from '../contracts/addresses.json';
 import Token from '../contracts/abis/Token.json';
 import Synthetix from '../contracts/abis/Synthetix.json';
+import ExchangeRates from '../contracts/abis/ExchangeRates.json';
 
 const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/c3725848586d482c91bba79cd74ca84a'));
 const account = web3.eth.accounts.privateKeyToAccount('0x697F36CEDBD6767265A15B938E7D246DCF4941574A0F12C2C36882A4992FBD9E');
@@ -17,9 +18,11 @@ web3.eth.accounts.wallet.add(account);
 
 const SETH = new web3.eth.Contract(Token, addresses.seth);
 const SUSD = new web3.eth.Contract(Token, addresses.susd);
+const SXAU = new web3.eth.Contract(Token, addresses.sxau);
 const SynthetixContract = new web3.eth.Contract(Synthetix, addresses.synthetix);
+const ExchangeRatesContract = new web3.eth.Contract(ExchangeRates, addresses.exchangeRates);
 
-const tokens = [
+const currencies = [
   {
     label: 'sETH',
     value: 'sETH',
@@ -28,12 +31,16 @@ const tokens = [
     label: 'sUSD',
     value: 'sUSD',
   },
+  {
+    label: 'sXAU',
+    value: 'sXAU',
+  },
 ];
 
 const Picker = ({ value, onChange }) => (
   <RNPickerSelect
     placeholder={{}}
-    items={tokens}
+    items={currencies}
     onValueChange={onChange}
     style={{
       ...pickerSelectStyles,
@@ -52,31 +59,82 @@ export default class HomeScreen extends Component {
   state = {
     sethBalance: '0',
     susdBalance: '0',
+    sxauBalance: '0',
     from: 'sETH',
     to: 'sUSD',
-    amount: '0',
+    fromAmount: '0',
+    rates: {},
+    toAmount: '0',
   }
   componentDidMount() {
     this.loadBalances();
+    this.loadRates();
   }
   loadBalances = async () => {
-    const [sethBalance, susdBalance] = await Promise.all([
+    const [sethBalance, susdBalance, sxauBalance] = await Promise.all([
       SETH.methods.balanceOf(account.address).call(),
       SUSD.methods.balanceOf(account.address).call(),
+      SXAU.methods.balanceOf(account.address).call(),
     ]);
     this.setState({
-      sethBalance: fromWei(sethBalance),
-      susdBalance: fromWei(susdBalance),
+      sethBalance: sethBalance,
+      susdBalance: susdBalance,
+      sxauBalance: sxauBalance,
     });
+  }
+  loadRates = async () => {
+    const rates = {};
+    await Promise.all(currencies.map(async item => {
+      if (item.value === 'sUSD') {
+        rates[item.value] = toWei('1');
+        return;
+      } 
+      const rate = await ExchangeRatesContract.methods.rateForCurrency(toHex(item.value)).call();
+      rates[item.value] = rate;
+    }));
+    this.setState({ rates });
   }
   swap = async () => {
     await SynthetixContract.methods.exchange(
       toHex(this.state.from),
-      toWei(this.state.amount),
+      toWei(this.state.fromAmount),
       toHex(this.state.to),
       account.address
     ).send({ from: account.address, gas: 1000000, gasPrice: 1000000000 });
     await this.loadBalances();
+  }
+  calculateAmount = (amount, from, to) => {
+    const amountToSwap = new BN(toWei(amount || '0'));
+    const fromRate = new BN(this.state.rates[from]);
+    const toRate = new BN(this.state.rates[to]);
+    return this.convert(amountToSwap.mul(fromRate).div(toRate));
+  }
+  updateToAmount = amount => {
+    const toAmount = this.calculateAmount(amount, this.state.from, this.state.to);
+    this.setState({ toAmount });
+  }
+  updateFromAmount = amount => {
+    const fromAmount = this.calculateAmount(amount, this.state.to, this.state.from);
+    this.setState({ fromAmount });
+  }
+  onFromAmountChange = value => {
+    this.setState({ fromAmount: value });
+    this.updateToAmount(value);
+  }
+  onToAmountChange = value => {
+    this.setState({ toAmount: value });
+    this.updateFromAmount(value);
+  }
+  onFromChange = value => {
+    this.setState({ fromAmount: '0', from: value });
+    this.updateToAmount('0');
+  }
+  onToChange = value => {
+    this.setState({ to: value });
+    this.updateToAmount(this.state.fromAmount);
+  }
+  convert = value => {
+    return Number(Number(fromWei(value)).toFixed(6)).toString();
   }
   render() {
     return (
@@ -89,26 +147,41 @@ export default class HomeScreen extends Component {
         <Content padder>
           <Grid>
             <Col>
-              <Text>sETH: {this.state.sethBalance}</Text>
-              <Text>sUSD: {this.state.susdBalance}</Text>
+              <Text>sETH: {this.convert(this.state.sethBalance)}</Text>
+              <Text>sUSD: {this.convert(this.state.susdBalance)}</Text>
+              <Text>sXAU: {this.convert(this.state.sxauBalance)}</Text>
             </Col>
           </Grid>
           <Grid style={{ marginTop: 20 }}>
             <Row>
               <Col>
-                <Picker value={this.state.from} onChange={value => this.setState({ from: value })} />
+                <Picker value={this.state.from} onChange={this.onFromChange} />
               </Col>
               <Col style={{ alignItems: 'center', justifyContent: 'center' }}>
                 <Icon type='Ionicons' name='arrow-round-forward' style={{ fontSize: 40 }} />
               </Col>
               <Col>
-                <Picker value={this.state.to} onChange={value => this.setState({ to: value })} />
+                <Picker value={this.state.to} onChange={this.onToChange} />
               </Col>
             </Row>
           </Grid>
-          <Item regular>
-            <Input keyboardType="decimal-pad" value={this.state.amount} onChangeText={value => this.setState({ amount: value })} />
-          </Item>
+          <Grid style={{ marginTop: 20 }}>
+            <Row>
+              <Col>
+                <Item regular>
+                  <Input keyboardType="decimal-pad" value={this.state.fromAmount} onChangeText={this.onFromAmountChange} />
+                </Item>
+              </Col>
+              <Col style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Icon type='Ionicons' name='menu' style={{ fontSize: 40 }} />
+              </Col>
+              <Col>
+                <Item regular>
+                  <Input keyboardType="decimal-pad" value={this.state.toAmount} onChangeText={this.onToAmountChange} />
+                </Item>
+              </Col>
+            </Row>
+          </Grid>
           <Button block style={{ marginTop: 20 }} onPress={this.swap}>
             <Text>Swap</Text>
           </Button>
