4e6f00867805ec8d974f0f8f3907e9a173026a61
Max Alekseenko <nnwen0609@gmail.com>
2019-10-26 15:01:10 +0300

    add exchange

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,208 +1,146 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, Button, Container, Header, Content, Body, Icon, Input, Item } from 'native-base';
+import RNPickerSelect from 'react-native-picker-select';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import Web3 from 'web3';
+import { fromWei, toHex } from 'web3-utils';
 
-import { SynthetixJs } from 'synthetix-js';
-const signer = new SynthetixJs.signers.PrivateKey(
-  null,
-  0,
-  '0x0123456789012345678901234567890123456789012345678901234567890123'
+import addresses from '../contracts/addresses.json';
+import Token from '../contracts/abis/Token.json';
+import Synthetix from '../contracts/abis/Synthetix.json';
+
+const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/c3725848586d482c91bba79cd74ca84a'));
+const account = web3.eth.accounts.privateKeyToAccount('0x697F36CEDBD6767265A15B938E7D246DCF4941574A0F12C2C36882A4992FBD9E');
+web3.eth.accounts.wallet.add(account);
+
+const SETH = new web3.eth.Contract(Token, addresses.seth);
+const SUSD = new web3.eth.Contract(Token, addresses.susd);
+const SynthetixContract = new web3.eth.Contract(Synthetix, addresses.synthetix);
+
+const tokens = [
+  {
+    label: 'sETH',
+    value: 'sETH',
+  },
+  {
+    label: 'sUSD',
+    value: 'sUSD',
+  },
+];
+
+const Picker = ({ value, onChange }) => (
+  <RNPickerSelect
+    placeholder={{}}
+    items={tokens}
+    onValueChange={onChange}
+    style={{
+      ...pickerSelectStyles,
+      iconContainer: {
+        top: 10,
+        right: 12,
+      },
+    }}
+    value={value}
+    useNativeAndroidPickerStyle={false}
+    textInputProps={{ underlineColor: 'yellow' }}
+  />
 );
-const snxjs = new SynthetixJs({ signer });
-console.log(snxjs);
 
-
-import { MonoText } from '../components/StyledText';
-
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
+export default class HomeScreen extends Component {
+  state = {
+    sethBalance: '0',
+    susdBalance: '0',
+    from: 'sETH',
+    to: 'sUSD',
+    amount: '0',
+  }
+  componentDidMount() {
+    this.loadBalances();
+  }
+  loadBalances = async () => {
+    const [sethBalance, susdBalance] = await Promise.all([
+      SETH.methods.balanceOf(account.address).call(),
+      SUSD.methods.balanceOf(account.address).call(),
+    ]);
+    this.setState({
+      sethBalance: fromWei(sethBalance),
+      susdBalance: fromWei(susdBalance),
+    });
+  }
+  swap = async () => {
+    await SynthetixContract.methods.exchange(
+      toHex(this.state.from),
+      toWei(this.state.amount),
+      toHex(this.state.to),
+      account.address
+    ).send({ from: account.address, gas: 1000000, gasPrice: 1000000000 });
+    await this.loadBalances();
+  }
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Text>Investix</Text>
+          </Body>
+        </Header>
+        <Content padder>
+          <Grid>
+            <Col>
+              <Text>sETH: {this.state.sethBalance}</Text>
+              <Text>sUSD: {this.state.susdBalance}</Text>
+            </Col>
+          </Grid>
+          <Grid style={{ marginTop: 20 }}>
+            <Row>
+              <Col>
+                <Picker value={this.state.from} onChange={value => this.setState({ from: value })} />
+              </Col>
+              <Col style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Icon type='Ionicons' name='arrow-round-forward' style={{ fontSize: 40 }} />
+              </Col>
+              <Col>
+                <Picker value={this.state.to} onChange={value => this.setState({ to: value })} />
+              </Col>
+            </Row>
+          </Grid>
+          <Item regular>
+            <Input keyboardType="decimal-pad" value={this.state.amount} onChangeText={value => this.setState({ amount: value })} />
+          </Item>
+          <Button block style={{ marginTop: 20 }} onPress={this.swap}>
+            <Text>Swap</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
   },
 });
