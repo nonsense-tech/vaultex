8cad452ca1fc6a3e32884713a5bb760ca8a00ec6
Max Alekseenko <nnwen0609@gmail.com>
2019-10-27 01:23:18 +0300

    add deposit way chooser

--- screens/DepositScreen.js
+++ screens/DepositScreen.js
@@ -6,11 +6,11 @@ import { Col, Row, Grid } from 'react-native-easy-grid';
 import addresses from '../contracts/addresses.json';
 
 export default class DepositScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showToast: false
-    };
+  state = {
+    depositWay: null,
+  }
+  chooseDepositWay = depositWay => {
+    this.setState({ depositWay });
   }
   render() {
     return (
@@ -20,21 +20,50 @@ export default class DepositScreen extends Component {
             <Text style={{ fontWeight: 'bold' }}>{'< VAULTEX >'}</Text>
           </Body>
         </Header>
-        <Content padder>
-          <Text
-            style={{ textAlign: 'center' }}
-          >
-            Send ETH to the next address and it will back you USD tokens
-          </Text>
-          <Text
-            style={{ textAlign: 'center', fontWeight: 'bold', marginTop: 20 }}
-            onPress={() => {
-              Clipboard.setString(addresses.uniswapExchanger);
-              Toast.show({ text: 'Copied to clipboard' });
-            }}
-          >
-            {addresses.uniswapExchanger}
-          </Text>
+        <Content padder style={{ paddingTop: 20 }}>
+          {!this.state.depositWay && (
+            <Col>
+              <Text style={{ textAlign: 'center' }}>
+                Choose the way to deposit
+              </Text>
+              <Button block style={{ marginTop: 20 }} onPress={() => this.chooseDepositWay('crypto')}>
+                <Text>Crypto</Text>
+              </Button>
+              <Button block style={{ marginTop: 20 }} onPress={() => this.chooseDepositWay('card')}>
+                <Text>Card</Text>
+              </Button>
+            </Col>
+          )}
+          {this.state.depositWay === 'crypto' && (
+            <Col>
+              <Text style={{ textAlign: 'center' }}>
+                Send ETH to the next address and it will back you USD tokens
+              </Text>
+              <Text
+                style={{ textAlign: 'center', fontWeight: 'bold', marginTop: 20 }}
+                onPress={() => {
+                  Clipboard.setString(addresses.uniswapExchanger);
+                  Toast.show({ text: 'Copied to clipboard' });
+                }}
+              >
+                {`${addresses.uniswapExchanger} `}
+                <Icon type='Ionicons' name='copy' style={{ fontSize: 20 }} />
+              </Text>
+              <Button block style={{ marginTop: 20 }} onPress={() => this.chooseDepositWay(null)}>
+                <Text>Cancel</Text>
+              </Button>
+            </Col>
+          )}
+          {this.state.depositWay === 'card' && (
+            <Col>
+              <Text style={{ textAlign: 'center' }}>
+                Not implemented yet
+              </Text>
+              <Button block style={{ marginTop: 20 }} onPress={() => this.chooseDepositWay(null)}>
+                <Text>Cancel</Text>
+              </Button>
+            </Col>
+          )}
         </Content>
       </Container>
     );
